0d6e4fb56778c82131bb27b434a5380edfb013a6
Vi Klaas <va.klaas@gmail.com>
2023-11-09 16:29:45 +0100

    closes #6: bugfix shiny server and enhancement Update deployment process and improve permissions handling
    This update modifies the deployment scripts, adds a process to tweak file permissions, and edits the main Readme to reflect these changes. We've introduced new scripts that standardize and automate the changing of permissions for specific use cases in the deployment process. This helps maintain robust security for files within the production environment. Also, we've made enhancements to the existing Shiny deployment script to automate tasks like creating directories and pulling latest code changes from git repositories. README has been updated to guide users through this adjusted process. Renaming of the start_shiny_dev.sh to start_shiny_test.sh provides a clearer indication of its purpose.
    
    A notable change in the Caddyfile is made to handle shiny-test prefix requests separately. In the Shiny app, we've introduced an app.R file for a minimal Shiny app. R settings files for JetBrains IDE have been included for those who use such an IDE. Overall, these updates aim to streamline and secure our deployment process.

---
+++ utils/set_permissions_deploymentscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Get the filename from command line arguments
+filename=$1
+
+# Check if a filename was provided
+if [ -z "$filename" ]; then
+    echo "Please provide a filename."
+    exit 1
+fi
+
+# Full path to the file
+filepath="/usr/local/bin/$filename"
+
+# Check if the file exists
+if [ ! -f "$filepath" ]; then
+    echo "File $filepath does not exist."
+    exit 1
+fi
+
+# Update permissions
+sudo chmod 750 "$filepath"
+
+echo "$filepath permissions have been updated: chmod 750."
